49f16bc93c1326912ca3c5098c260721171c7dfe
DaniVezaro <Daniela.raithz@outlook.com>
2025-03-19 23:45:02 -0300

    Resolvido exercicio de soma

--- src/exercicio02/Soma.java
+++ src/exercicio02/Soma.java
@@ -4,6 +4,12 @@ public class Soma {
 
 	public static void main(String[] args) {
 		
+		int n1=  5;
+        int n2= 10;
+        
+        int soma = 5 + 10;
+         
+        System.out.print("O resultado da soma é: " + soma);	
 	}
 
 }
